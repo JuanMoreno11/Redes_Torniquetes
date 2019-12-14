e7f92abf99f23d623df91a0be5effb3187728a42
Tatiana Velasco R <50845610+tativelasco@users.noreply.github.com>
2019-12-14 10:12:01 -0500

    debugs loop
    si no hay taps para un año-porteria-accion-dia, entonces pasa al siguiente sin parar el loop.

--- Code/interactions_all_uniandes.R
+++ Code/interactions_all_uniandes.R
@@ -43,56 +43,56 @@ start_time <- Sys.time() # Start counting time
  #### ENERO 
  # --------
 
-setwd(source)
-mydata <- read.csv("enero.csv")
-
-# Format data
-mydata <- mydata[mydata$modo == "Peatonal",]
-mydata <- mydata[!is.na(mydata$carnet), ]
-mydata <- mydata[, -which(names(mydata) %in%  c("X", "programa", "edificio", "modo", "dia", "NA.", "jornada", "fecha", "mesn"))] # Remove NA row
-mydata <- mydata[!duplicated(mydata),] # Drop duplicates
-
-# Format variables variables
-mydata$date_time <- strptime(mydata$fecha_completa, format = '%Y.%m.%d %H:%M:%S')
-mydata$torniquete <- as.character(mydata$torniquete)
-mydata$year <- year(mydata$date_time)
-mydata$porteria <- trimws(as.character(mydata$porteria))
-mydata$accion <- as.character(mydata$accion)
-mydata$action_in <- ifelse(mydata$accion == "IN ", 1, ifelse(mydata$accion == "OUT", 0, NA))    
-
-
-# gen auxiliary tools
-building <- c("W", "SD", "S", "RGB", "PB", "ÑF", "ÑE", "NAVAS", "ML", "MJ", "LL", "GA", "FRANCO", "CPM", "CORCAS", "CAI", "AU")
-# building <- c("SD") # For testing
-
-
-# Genera edge list
-# ----------------
-# Create auxiliary tools
-edge.list.full.all <- matrix(nrow = 1, ncol = 9, rep(NA))
-colnames(edge.list.full.all) <- c("carnet1", "carnet2", "date_time_carnet1", "torniquete_carnet1", "torniquete_carnet2", "porteria", "action", "day", "year")
-
-for (y in 2016:2019) { # try w/one year
-  for (b in building) { 
-    for (a in 0:1) { # try outs only
-      for (w in 1:5)  { # try mondays only
-        
-       my.data.filtered <- mydata[mydata$year == y, ]
-       my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
-       edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
-       edge.list.full.all <- rbind(edge.list.full.all, edge.list)
-      }
-    }
-  }
-  
-}
-
-setwd(store)
-edge.list.full.enero.csv <- write.csv2(edge.list.full.all, "edge.list.full.enero.csv")
+# setwd(source)
+# mydata <- read.csv("enero.csv")
+# 
+# # Format data
+# mydata <- mydata[mydata$modo == "Peatonal",]
+# mydata <- mydata[!is.na(mydata$carnet), ]
+# mydata <- mydata[, -which(names(mydata) %in%  c("X", "programa", "edificio", "modo", "dia", "NA.", "jornada", "fecha", "mesn"))] # Remove NA row
+# mydata <- mydata[!duplicated(mydata),] # Drop duplicates
+# 
+# # Format variables variables
+# mydata$date_time <- strptime(mydata$fecha_completa, format = '%Y.%m.%d %H:%M:%S')
+# mydata$torniquete <- as.character(mydata$torniquete)
+# mydata$year <- year(mydata$date_time)
+# mydata$porteria <- trimws(as.character(mydata$porteria))
+# mydata$accion <- as.character(mydata$accion)
+# mydata$action_in <- ifelse(mydata$accion == "IN ", 1, ifelse(mydata$accion == "OUT", 0, NA))    
+# 
+# 
+# # gen auxiliary tools
+# building <- c("W", "SD", "S", "RGB", "PB", "ÑF", "ÑE", "NAVAS", "ML", "MJ", "LL", "GA", "FRANCO", "CPM", "CORCAS", "CAI", "AU")
+# # building <- c("SD") # For testing
+# 
+# 
+# # Genera edge list
+# # ----------------
+# # Create auxiliary tools
+# edge.list.full.all <- matrix(nrow = 1, ncol = 9, rep(NA))
+# colnames(edge.list.full.all) <- c("carnet1", "carnet2", "date_time_carnet1", "torniquete_carnet1", "torniquete_carnet2", "porteria", "action", "day", "year")
+# 
+# for (y in 2016:2019) { # try w/one year
+#   for (b in building) { 
+#     for (a in 0:1) { # try outs only
+#       for (w in 1:5)  { # try mondays only
+#         
+#       my.data.filtered <- mydata[mydata$year == y, ]
+#       my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+#       if(nrow(my.data.day) == 0) {next}
+#       edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
+#       edge.list.full.all <- rbind(edge.list.full.all, edge.list)
+#       }
+#     }
+#   }
+#   
+# }
+# 
+# setwd(store)
+# edge.list.full.enero.csv <- write.csv2(edge.list.full.all, "edge.list.full.enero.csv")
 
 #### FEBRERO 
 # --------
-
 setwd(source)
 mydata <- read.csv("febrero.csv")
 
@@ -129,6 +129,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2) # Decirle a la funcion qué distancia es permitida
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -140,6 +141,9 @@ for (y in 2016:2019) { # try w/one year
 setwd(store)
 edge.list.full.febrero.csv <- write.csv2(edge.list.full.all, "edge.list.full.febrero.csv")
 
+end_time <- Sys.time() # stop counting time
+time <- end_time - start_time
+time 
 #### MARZO 
 # --------
 
@@ -179,6 +183,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -230,6 +235,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -280,6 +286,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -330,6 +337,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -380,6 +388,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -430,6 +439,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
@@ -480,6 +490,7 @@ for (y in 2016:2019) { # try w/one year
         
         my.data.filtered <- mydata[mydata$year == y, ]
         my.data.day <- my.data.filtered[ which(my.data.filtered$porteria==b & my.data.filtered$action_in == a & my.data.filtered$dia_semana == w), ] 
+        if(nrow(my.data.day) == 0) {next}
         edge.list <- all.full.edgelist(my.data.day, building = b, action=a, day=w, year=y, time = 2)
         edge.list.full.all <- rbind(edge.list.full.all, edge.list)
       }
